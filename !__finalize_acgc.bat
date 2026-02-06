81b8663bbf5c6dfb5d82bf1b753c383234ca93a9
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-02-05 20:24:05 -0600

    Add files

---
+++ !__finalize_acgc.bat
@@ -0,0 +1,12 @@
+echo off
+echo Copying foresta.rel.szs...
+copy /Y %cd%\build\GAFE01_00\foresta\foresta.rel.szs iso\__root\foresta.rel.szs
+echo Copying static.dol to Start.dol...
+copy /Y %cd%\build\GAFE01_00\static.dol iso\__root\^&^&systemdata\Start.dol
+echo Building ISO...
+cd iso
+gc_fst rebuild "root" "Animal Crossing (Latest).iso"
+cd ..
+echo Complete^!
+pause
+exit
